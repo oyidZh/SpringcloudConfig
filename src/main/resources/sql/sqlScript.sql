dc8566b9751d3d2b23d80609c2d554fdcca9be2f
liulinhui <1249055292@12345>
2017-08-07 14:10:57 +0800

    sql

--- src/main/resources/sql/sqlScript.sql
+++ src/main/resources/sql/sqlScript.sql
@@ -25,3 +25,16 @@ CREATE TABLE IF NOT EXISTS `JUDGE_USER`(
   `ability_level` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for scheduleTime
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `JUDGE_SCHEDULE_TIME`(
+  `course_name` varchar(20) DEFAULT NULL,
+  `course_code` varchar(20) NOT NULL,
+  `start_time` DATETIME DEFAULT NULL,
+  `commit_end_time` DATETIME DEFAULT NULL,
+  `finish_time` DATETIME DEFAULT NULL,
+  `publish_time` DATETIME DEFAULT NULL,
+  PRIMARY KEY (`course_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

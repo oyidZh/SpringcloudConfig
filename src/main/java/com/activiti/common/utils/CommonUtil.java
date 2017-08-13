7c0cbb85b5247962c741d7c839360b59e87bfb92
liulinhui <1249055292@qq.com>
2017-08-13 23:09:42 +0800

    流程图

---
+++ src/main/java/com/activiti/common/utils/CommonUtil.java
@@ -0,0 +1,50 @@
+package com.activiti.common.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+
+/**
+ * 共同帮助类
+ * Created by 12490 on 2017/8/13.
+ */
+@Component
+public class CommonUtil {
+    private static final Logger logger = LoggerFactory.getLogger(CommonUtil.class);
+
+    /**
+     * 请求gitlab获取题目与答案
+     * @param url
+     * @return
+     */
+    public JSONObject getQAFromGitLab(String url){
+        //get请求返回结果
+        JSONObject jsonResult = null;
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet(url);
+            HttpResponse response = client.execute(request);
+
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String strResult = EntityUtils.toString(response.getEntity());
+                jsonResult = JSONObject.parseObject(strResult);
+                url = URLDecoder.decode(url, "UTF-8");
+            } else {
+                logger.error("get请求提交失败:" + url);
+            }
+        } catch (IOException e) {
+            logger.error("get请求提交失败:" + url, e);
+        }
+        return jsonResult;
+    }
+}

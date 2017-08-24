f0965dc8ce3f3fedb6385646d4fe793127cec60d
liulinhui <1249055292@qq.com>
2017-08-24 22:46:18 +0800

    优化学生分配  根据随机生成的userName重新排序

--- src/main/java/com/activiti/common/utils/CommonUtil.java
+++ src/main/java/com/activiti/common/utils/CommonUtil.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.Random;
 
 /**
  * 共同帮助类
@@ -75,5 +76,30 @@ public class CommonUtil {
         } else if (emailDto.getType() == EmailType.resource) {
             mailService.sendInlineResourceMail(emailDto.getAddress(), emailDto.getSubject(), emailDto.getContent(), emailDto.getRscPath(), emailDto.getRscId());
         }
+    }
+
+    /**
+     * 生成随机数
+     *
+     * @param length 表示生成字符串的长度
+     * @return
+     */
+    public String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 生成随机用户名用来打乱用户
+     * @return
+     */
+    public String getRandomUserName() {
+        return getRandomString(10);
     }
 }
